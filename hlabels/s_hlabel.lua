3cd784a2ea188b374df183242b267350a36f32e1
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-15 17:55:28 +0100

    send a message to the client once the range has been changed

--- hlabels/s_hlabel.lua
+++ hlabels/s_hlabel.lua
@@ -1,4 +1,6 @@
+-- a colour to show there is a problem
 col_err = {238, 167, 74}
+col_succ = {114, 210, 80}
 
 RegisterCommand('hlrange', function(source, args)
     if #args < 1 then
@@ -6,15 +8,27 @@ RegisterCommand('hlrange', function(source, args)
             args = {"Head Labels", "Invalid amount of arguments provided."},
             color = col_err
         })
+        return
     end
        
     if args[1] == 'c' then
         TriggerClientEvent('setHeadLabelDistance', source, 15)
-        -- maybe notify the client also :shrug:
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {"Head Labels", "You have set your head label display range to close."},
+            color = col_succ
+        })
     elseif args[1] == 'm' then
         TriggerClientEvent('setHeadLabelDistance', source, 200)
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {"Head Labels", "You have set your head label display range to medium."},
+            color = col_succ
+        })
     elseif args[1] == 'f' then
         TriggerClientEvent('setHeadLabelDistance', source, 10000)
+        TriggerClientEvent('chat:addMessage', source, {
+            args = {"Head Labels", "You have set your head label display range to far."},
+            color = col_succ
+        })
     else
         TriggerClientEvent('chat:addMessage', source, {
             args = {"Head Labels", "Unknown distance provided."},
