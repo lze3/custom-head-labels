d8ba2a996accd2d3690d6e83a9ef42f58a742b61
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-15 18:15:06 +0100

    add a version check

--- hlabels/__resource.lua
+++ hlabels/__resource.lua
@@ -1,4 +1,7 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_script "c_hlabel.lua"
-server_script "s_hlabel.lua"
+server_scripts { 
+    "s_hlabel.lua",
+    "versioncheck.lua"
+}
---
+++ hlabels/versioncheck.lua
@@ -0,0 +1,20 @@
+local curVersion = json.decode(LoadResourceFile(GetCurrentResourceName(), "version.json")).version
+Citizen.CreateThread(function()
+    PerformHttpRequest("https://raw.githubusercontent.com/JHodgson1/custom-head-labels/master/version.json", function(err, response, header)
+        local data = json.decode(response)
+
+        if curVersion ~= data.version and tonumber(curVersion) < tonumber(data.version) then
+            print("\n^1##################################")
+            print(GetCurrentResourceName() .. " is outdated, please consider updating.")
+            print("Current Version: " .. data.version)
+            print("Your Version: " .. curVersion)
+            print("Update Change Log:\n^5" .. data.changelog)
+            print("\n^1##################################\n^0")
+        elseif tonumber(curVersion) > tonumber(data.version) then
+            print("Your version of " .. GetCurrentResourceName() .. " appears to be higher than the latest stable version, consider downgrading to the latest " .. data.version .. " you may be on an unstable version!")
+        else
+            print("Congratulations! Your version of " .. GetCurrentResourceName() .. " is correct and up-to-date, you're awesome!")
+        end
+
+    end, "GET", "", {version = 'this'})
+end)
