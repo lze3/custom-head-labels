7f806eca25bdb3e803755a530c189fdd7d414c5c
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-19 21:12:49 +0100

    better colouring method

--- hlabels/s_hlabel.lua
+++ hlabels/s_hlabel.lua
@@ -1,12 +1,16 @@
--- a colour to show there is a problem
-col_err = {238, 167, 74}
-col_succ = {114, 210, 80}
+-- colours that determine the success level
+colours = 
+{
+    ["error"] = {238, 167, 74},
+    ["success"] = {114, 210, 80}
+}
+
 
 RegisterCommand('hlrange', function(source, args)
     if #args < 1 then
         TriggerClientEvent('chat:addMessage', source, {
             args = {"Head Labels", "Invalid amount of arguments provided."},
-            color = col_err
+            color = colours.error
         })
         return
     end
@@ -15,24 +19,24 @@ RegisterCommand('hlrange', function(source, args)
         TriggerClientEvent('setHeadLabelDistance', source, 15)
         TriggerClientEvent('chat:addMessage', source, {
             args = {"Head Labels", "You have set your head label display range to close."},
-            color = col_succ
+            color = colours.success
         })
     elseif args[1] == 'm' then
         TriggerClientEvent('setHeadLabelDistance', source, 200)
         TriggerClientEvent('chat:addMessage', source, {
             args = {"Head Labels", "You have set your head label display range to medium."},
-            color = col_succ
+            color = colours.success
         })
     elseif args[1] == 'f' then
         TriggerClientEvent('setHeadLabelDistance', source, 10000)
         TriggerClientEvent('chat:addMessage', source, {
             args = {"Head Labels", "You have set your head label display range to far."},
-            color = col_succ
+            color = colours.success
         })
     else
         TriggerClientEvent('chat:addMessage', source, {
             args = {"Head Labels", "Unknown distance provided."},
-            color = col_err
+            color = colours.error
         })
     end
 end, true)
