ac56f32a73671541148a68705588f2290a60b096
Jack Hodgson <hodgsonjack@hotmail.com>
2019-05-20 22:25:41 +0100

    MISC: better file structure

--- hlabels/__resource.lua
+++ hlabels/__resource.lua
@@ -1,7 +1,6 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
-client_script "c_hlabel.lua"
-server_scripts { 
-    "s_hlabel.lua",
-    "versioncheck.lua"
-}
+client_script "client/c_hlabel.lua"
+
+server_script "server/s_hlabel.lua"
+server_script "versioncheck.lua"
