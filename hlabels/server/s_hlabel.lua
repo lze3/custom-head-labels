71bccb03a09e5a598617ec00274f68d8effcc7a5
Jack Hodgson <hodgsonjack@hotmail.com>
2019-05-24 23:18:10 +0100

    better code

--- hlabels/server/s_hlabel.lua
+++ hlabels/server/s_hlabel.lua
@@ -5,38 +5,63 @@ colours =
     ["success"] = {114, 210, 80}
 }
 
+distances = {
+    ["close"] = {
+        ["shortname"] = "C",
+        ["name"] = "CLOSE",
+        ["distance"] = 15
+    },
+
+    ["med"] = {
+        ["shortname"] = "M",
+        ["name"] = "MEDIUM",
+        ["distance"] = 200
+    },
+
+    ["far"] = {
+        ["shortname"] = "F",
+        ["name"] = "FAR",
+        ["distance"] = -1
+    }
+}
 
 RegisterCommand('hlrange', function(source, args)
-    if #args < 1 then
-        TriggerClientEvent('chat:addMessage', source, {
-            args = {"Head Labels", "Invalid amount of arguments provided."},
-            color = colours.error
-        })
-        return
-    end
-       
-    if args[1] == 'c' then
-        TriggerClientEvent('setHeadLabelDistance', source, 15)
-        TriggerClientEvent('chat:addMessage', source, {
-            args = {"Head Labels", "You have set your head label display range to close."},
-            color = colours.success
-        })
-    elseif args[1] == 'm' then
-        TriggerClientEvent('setHeadLabelDistance', source, 200)
-        TriggerClientEvent('chat:addMessage', source, {
-            args = {"Head Labels", "You have set your head label display range to medium."},
-            color = colours.success
-        })
-    elseif args[1] == 'f' then
-        TriggerClientEvent('setHeadLabelDistance', source, 10000)
-        TriggerClientEvent('chat:addMessage', source, {
-            args = {"Head Labels", "You have set your head label display range to far."},
-            color = colours.success
-        })
+    if IsPlayerAceAllowed(source, "hlabels.hlrange") then
+        if #args < 1 then
+            TriggerClientEvent('chat:addMessage', source, {
+                args = {"Head Labels", "Invalid amount of arguments provided."},
+                color = colours.error
+            })
+            return
+        end
+
+        local shouldRun
+        local range
+        for k, v in pairs(distances) do
+            if v["shortname"]:lower() == args[1] then
+                shouldRun = true
+                range = v
+                break
+            end
+        end
+
+        if shouldRun then 
+            TriggerClientEvent("setHeadLabelDistance", source, range.distance)
+            TriggerClientEvent("chat:addMessage", source, {
+                args = {"Head Labels", "You have set your head label display range to: [ " .. range.name:upper() .. " ]."},
+                color = colours.success
+            })
+            shouldRun = not shouldRun
+        else
+            TriggerClientEvent("chat:addMessage", source, {
+                args = {"Head Labels", "Unknown distance provided."},
+                color = colours.error
+            })
+        end
     else
-        TriggerClientEvent('chat:addMessage', source, {
-            args = {"Head Labels", "Unknown distance provided."},
+        TriggerClientEvent("chat:addMessage", source, {
+            args = {"Head Labels", "You do not have permission to execute this command (hlrange)."},
             color = colours.error
         })
     end
-end, true)
+end, false)
